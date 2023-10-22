c5ec34fdf198b0ad1d025f4ca350a58e80ad830b
Daniel Schreurs <daniel.schreurs@hotmail.com>
2023-10-22 16:53:36 +0200

    :tada: Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:dto/team.dart';
+import 'package:dto/user.dart';
+import 'package:firedart/firedart.dart';
+import 'package:load_data_to_firestore/config.dart';
+import 'package:load_data_to_firestore/teams.dart';
+import 'package:load_data_to_firestore/users.dart';
+
+String pi = PI;
+String apiKey = API_KEY;
+
+void main(List<String> arguments) async {
+  Firestore.initialize(pi);
+  late TokenStore tokenStore;
+
+  tokenStore = VolatileStore();
+  FirebaseAuth(apiKey, tokenStore);
+  FirebaseAuth.initialize(apiKey, tokenStore);
+  await addUsers();
+
+  await addTeams();
+}
+
+Future<void> addTeams() async {
+  for (Team team in teams) {
+    Firestore.instance.collection('teams').add(team.toJson());
+  }
+}
+
+Future<void> addUsers() async {
+  for (User user in users) {
+    Firestore.instance
+        .collection('users')
+        .document(user.email)
+        .set(user.toJson());
+    try {
+      await FirebaseAuth.instance.signUp(user.email, '1234567890');
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
